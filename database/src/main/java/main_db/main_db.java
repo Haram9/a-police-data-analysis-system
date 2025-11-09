768a1738d92d3127c2c1c12a06a7ceea81fb3205
DELL <DELL@DESKTOP-M42CDH1>
2025-11-09 18:33:12 +0500

    test push

---
+++ database/src/main/java/main_db/main_db.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package main_db;
+
+/**
+ * 
+ */
+public class main_db {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	   System.out.print("test");
+	}
+
+}
