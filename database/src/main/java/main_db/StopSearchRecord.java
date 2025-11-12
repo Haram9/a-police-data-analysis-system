39b0f4cf5afeceaa4e0be22b82587f8e0c2ef32f
sadia <sadia@HP>
2025-11-12 11:49:53 +0500

    1st edit

--- database/src/main/java/main_db/StopSearchRecord.java
+++ database/src/main/java/main_db/StopSearchRecord.java
@@ -1,24 +1,20 @@
 package main_db;
+
 import java.time.LocalDateTime;
 
 public class StopSearchRecord {
-    String type;
-    LocalDateTime date;
-    String gender;
-    String ageRange;
-    String selfDefinedEthnicity;
-    String officerDefinedEthnicity;
-    String legislation;
-    String objectOfSearch;
-    String outcome;
-    Boolean outcomeLinked;
-    Boolean removalOfClothing;
-    Double latitude;
-    Double longitude;
 
-   
-    public String toString() {
-        return String.format("[%s] %s, %s, %s, %s, Legislation: %s, Outcome: %s",
-            date, gender, ageRange, objectOfSearch, selfDefinedEthnicity, legislation, outcome);
-    }
+    public String type;
+    public LocalDateTime date;
+    public String gender;
+    public String ageRange;
+    public String selfDefinedEthnicity;
+    public String officerDefinedEthnicity;
+    public String legislation;
+    public String objectOfSearch;
+    public String outcome;
+    public Boolean outcomeLinked;
+    public Boolean removalOfClothing;
+    public double latitude;
+    public double longitude;
 }
